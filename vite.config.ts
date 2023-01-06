91bd4f19755780d707a5fc091f5c019d9052f0cb
BIYA Paul <bpsmartdesign@hotmail.com>
2023-01-06 09:46:46 +0100

    refactor: Review core configs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.*)$/,
-        replacement: "node_modules/$1",
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "RRWebPlayer",
+      fileName: '@bpsmartdesign/rrweb-player-vue3',
+    },
+    rollupOptions: {
+      external: ["vue", "rrweb"],
+      output: {
+        globals: {
+          vue: "vue",
+          rrweb: "rrweb",
+        },
       },
-    ],
+    },
   },
+  plugins: [vue()],
 });
