b29a5676258699956f3813afff9a76f327741304
BIYA Paul <bpsmartdesign@hotmail.com>
2023-01-06 05:45:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)$/,
+        replacement: "node_modules/$1",
+      },
+    ],
+  },
+});
