62c5283b47fca8ff58937878c169944bcbd0d516
BIYA Paul <bpsmartdesign@hotmail.com>
2023-03-27 18:16:20 +0100

    release: v1.0.8.

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
 import { ref } from "vue";
 import Player from "./../lib/components/Player.vue";
 import events from "./../dev/data";
-// import { events2 } from "../dev/events-2";
+import { events2 } from "../dev/events-2";
 
 const _events = ref<any[]>(events);
-// const _events3 = ref<any[]>(events2);
+const _events3 = ref<any[]>(events2);
 </script>
 
 <template>
   <div>
-    <Player :events="_events" :width="900" :height="350" :auto-play="true" /> <br />
+    <!-- <Player :events="_events" :width="900" :height="350" :auto-play="true" /> <br /> -->
+    <Player
+      :events="_events3"
+      :width="800"
+      :height="350"
+      :go-to="15000"
+      :auto-play="false"
+    />
   </div>
-  <!-- <Player :events="_events3" :width="800" :height="350" :go-to="15000" :auto-play="false" /> -->
 </template>
