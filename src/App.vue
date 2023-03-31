b41c2c2522fb1ba5b85fb945d130b170b5ba0ae4
BIYA Paul <bpsmartdesign@hotmail.com>
2023-03-31 04:08:52 +0100

    feat: Try CORS issues bypass.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted, nextTick } from "vue";
 import Player from "./../lib/components/Player.vue";
 import events from "./../dev/data";
 import { events2 } from "../dev/events-2";
 
 const _events = ref<any[]>(events);
 const _events3 = ref<any[]>(events2);
+
+onMounted(async () => {
+  if (_events3.value) {
+    await nextTick()
+    const _iframe = document.querySelector('iframe') as HTMLIFrameElement
+    if (_iframe) {
+      _iframe.setAttribute('allow', 'autoplay')
+      _iframe.setAttribute('crossorigin', 'anonymous')
+      _iframe.setAttribute('title', 'is-rrweb-player')
+      
+      console.log(': ', _iframe)
+    }
+  }
+})
 </script>
 
 <template>
@@ -16,7 +30,7 @@ const _events3 = ref<any[]>(events2);
       :width="800"
       :height="350"
       :go-to="15000"
-      :auto-play="false"
+      :auto-play="true"
     />
   </div>
 </template>
